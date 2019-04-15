3d80456756523c16b6bd5c4ad365053c4d8826d4
jason19901028 <jason_dlb@sina.cn>
2019-04-15 23:09:33 +0800

    update

--- server/db.js
+++ server/db.js
@@ -1,29 +1,40 @@
 let mysql = require('mysql')
-
+console.log('mysql=', mysql)
 const mysqlConfig = {
+  // host: '192.168.4.80',
+  // user: 'root',
+  // password: 'bakheet001',
+  // database: 'daitest',
   host: 'localhost',
   user: 'root',
   password: '123456',
-  database: 'mysql',
+  database: 'technologyblog',
   port: '3306'
 }
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: mysqlConfig.host,
   user: mysqlConfig.user,
   password: mysqlConfig.password,
-  database: mysqlConfig.database,
-  port: mysqlConfig.port,
-  multipleStatements: true
+  database: mysqlConfig.database
 })
+// console.log('pool=', pool)
+// let db = mysql.createConnection(mysqlConfig)
+// db.connect()
+// exports.do = function (sql, c)
+connection.connect()
+let sql = 'INSERT INTO blog(id, name) VALUES (null, "blog6")'
+connection.query(sql, (err, result) => {
+  console.log('err=', err)
+  console.log('result=', result)
+})
+// let setValue = function () {
+//   pool.getConnection((err, connection) => {
+//     let sql = 'INSERT INTO blog(id, name) VALUES (null, "blog")'
+//     connection.query(sql,(err, result)=>{
+//       console.log(result)
+//       connection.release()
+//     })
+//   })
+// }
 
-let setValue = function () {
-  pool.getConnection((err, connection) => {
-    let sql = 'INSERT INTO blog(id, name) VALUES (1, "blog")'
-    connection.query(sql,(err, result)=>{
-      console.log(result)
-      connection.release()
-    })
-  })
-}
-
-setValue()
+// setValue()
