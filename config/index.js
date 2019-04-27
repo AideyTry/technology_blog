7492783b8fc8078b83a8ad6173543d8f0e1eec7f
jason19901028 <jason_dlb@sina.cn>
2019-04-27 16:09:27 +0800

    端口修改

--- config/index.js
+++ config/index.js
@@ -18,8 +18,8 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: 'http://106.14.127.10/', // can be overwritten by process.env.HOST
+    port: 10086, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
